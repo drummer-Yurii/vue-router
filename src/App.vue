6a177674a3d54bcb7493a67a9d97c9b6a18cb071
Yurii Rybachok <drummer2009@ukr.net>
2023-05-22 15:43:27 +0300

    create Navbar component & Loading for movies

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto">
-    <h1>Header</h1>
+    <Navbar />
     <RouterView />
   </div>
 </template>
